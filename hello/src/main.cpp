183097dfbec6c5f28064abca9e37cfefd5092a3b
raaveinm <kirillraaveinm@gmail.com>
2026-06-18 19:37:46 +0300

    Setup Conan integration and add Boost.Asio TCP examples
    - Add `conan_clion_setup.cmake` for automatic Conan build execution in CLion.
    - Integrate the new Conan setup script and update generators in `conanfile.txt`.
    - Add a new `tcp-udp` sample project in `sprint1/samples/tcp-udp`

--- hello/src/main.cpp
+++ hello/src/main.cpp
@@ -9,6 +9,7 @@
 #include <boost/asio.hpp>
 #include <chrono>
 #include <iostream>
+#include <memory>
 #include <thread>
 
 namespace net = boost::asio;
@@ -17,19 +18,52 @@ using namespace std::chrono;
 using namespace std::literals;
 
 int main() {
-    // Шаблон boost::optional — прообраз std::optional, используем его здесь для примера
     boost::optional<int> opt;
     std::cout << opt << std::endl;
 
     net::io_context io;
+    const auto start_time = steady_clock::now();
+    {
+        auto t = std::make_shared<net::steady_timer>(io, 3s);
+        std::cout << "Fry eggs"sv << std::endl;
+        t->async_wait([t](sys::error_code ec) {
+            if (ec) {
+                throw std::runtime_error("Wait error: "s + ec.message());
+            }
+            std::cout << "Put eggs onto the plate. Thread id: "sv << std::this_thread::get_id() << std::endl;
+        });
+    }
+
+    {
+        auto t = std::make_shared<net::steady_timer>(io, 5s);
+        std::cout << "Brew coffee"sv << std::endl;
+        t->async_wait([t](sys::error_code ec) {
+            if (ec) {
+                throw std::runtime_error("Wait error: "s + ec.message());
+            }
+            std::cout << "Pour coffee in the cup. Thread id: "sv << std::this_thread::get_id()
+                      << std::endl;
+        });
+    }
+
+    try {
+        std::cout << "Run asynchronous operations"sv << std::endl;
+        io.run();
+        const auto cook_duration = duration<double>(steady_clock::now() - start_time);
+        std::cout << "Breakfast has been cooked in "sv << cook_duration.count() << "s"sv << std::endl;
+        std::cout << "Thread id: "sv << std::this_thread::get_id() << std::endl;
+        std::cout << "Enjoy your meal"sv << std::endl;
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
 }
 
 // cd hello/build
 // source ../../.venv/bin/activate
 // export CMAKE_POLICY_VERSION_MINIMUM=3.5
 // conan install .. --build=missing
-
+//
 // cmake .. -DCMAKE_BUILD_TYPE=Release
 // cmake --build .
-
+//
 // ./bin/hello
---
+++ sprint1/samples/tcp-udp/tcp_client.cpp
@@ -0,0 +1,62 @@
+//
+// Created by raaveinm on 6/18/26.
+//
+#include <iostream>
+#include <boost/asio.hpp>
+
+namespace net = boost::asio;
+using net::ip::tcp;
+
+int main(const int argc, char *argv[]) {
+    static constexpr int PORT = 3333;
+
+    if (argc != 2) {
+        std::cerr << "Usage: "<< argv[0] <<" <server_ip>" << std::endl;
+        return 1;
+    }
+
+    // create endpoint data
+    boost::system::error_code error;
+    const auto endpoint = tcp::endpoint(net::ip::make_address(argv[1], error), PORT);
+
+    if (error) {
+        std::cerr << "Could not connect to " << endpoint << std::endl;
+        return 2;
+    }
+
+    // connect to server
+    net::io_context io_context;
+    tcp::socket socket(io_context);
+    socket.connect(endpoint, error);
+
+    if (error) {
+        std::cerr << "Could not connect to " << endpoint << std::endl;
+        return 3;
+    }
+
+    // boost::asio::write - await all data to be sent
+    // boost::asio::write_some - at least one byte sent
+    // boost::asio::async_write - add data to queue and forget about it
+    socket.write_some(net::buffer("Bonjour, is anyone there?\n"), error);
+
+    if (error) {
+        std::cerr << "Can't write data" << std::endl;
+        return 4;
+    }
+
+    net::streambuf stream_buf;
+    net::read_until(socket, stream_buf, '\n', error);
+    std::string server_answer{
+            std::istreambuf_iterator(&stream_buf),
+            std::istreambuf_iterator<char>()
+    };
+
+    if (error) {
+        std::cerr << "Can't read data" << std::endl;
+        return 5;
+    }
+
+    std::cout << "__server_answer >>> " << server_answer << std::endl;
+
+    return 0;
+}
