9d6bcb0d66985a177fa9fb64adddb6ed04ea5659
raaveinm <kirillraaveinm@gmail.com>
2026-06-25 14:31:23 +0300

    Completed cafeteria task

--- hello/src/hamburger.cpp
+++ hello/src/hamburger.cpp
@@ -1,7 +1,12 @@
 //
 // Created by raaveinm on 6/25/26.
 //
+#include <ranges>
+#ifdef WIN32
+#include <sdkddkver.h>
+#endif
 
+#include <thread>
 #include <chrono>
 #include <functional>
 #include <iostream>
@@ -9,9 +14,28 @@
 #include <string_view>
 #include <syncstream>
 #include <boost/asio.hpp>
+#include <atomic>
 
 namespace net = boost::asio;
 namespace sys = boost::system;
+
+class ThreadChecker {
+public:
+    explicit ThreadChecker(std::atomic_int& counter)
+        : counter_{counter} {
+    }
+
+    ThreadChecker(const ThreadChecker&) = delete;
+    ThreadChecker& operator=(const ThreadChecker&) = delete;
+
+    ~ThreadChecker() {
+        assert(expected_counter_ == counter_);
+    }
+
+private:
+    std::atomic_int& counter_;
+    int expected_counter_ = ++counter_;
+};
 
 class Hamburger {
 public:
@@ -41,8 +65,9 @@ public:
 
 private:
     void AssureCutletRoasted() const {
-        if (!cutlet_roasted_)
-            throw std::logic_error("Cutlet has not been roasted yet");
+        if (!cutlet_roasted_) {
+            throw std::logic_error("Bread has not been roasted yet");
+        }
     }
 
     bool cutlet_roasted_ = false;
@@ -58,18 +83,19 @@ std::ostream& operator<<(std::ostream& os, const Hamburger& h) {
 
 class Logger {
 public:
-    explicit Logger(std::string id) : id_(std::move(id)) {
+    explicit Logger(std::string id)
+        : id_(std::move(id)) {
     }
 
     void LogMessage(const std::string_view message) const {
-        std::osyncstream os(std::cout);
+        std::osyncstream os{std::cout};
         os << id_ << "> [" << std::chrono::duration<double>(std::chrono::steady_clock::now() - start_time_).count()
-   << "s] " << message << std::endl;
+           << "s] " << message << std::endl;
     }
 
 private:
     std::string id_;
-    std::chrono::steady_clock::time_point start_time_;
+    std::chrono::steady_clock::time_point start_time_{std::chrono::steady_clock::now()};
 };
 
 using OrderHandler = std::function<void(sys::error_code err, int id, Hamburger* hamburger)>;
@@ -92,9 +118,10 @@ public:
 private:
     void RoastCutlet() {
         logger_.LogMessage("Start roasting cutlet");
-        roast_timer_.async_wait([self = shared_from_this()](sys::error_code ec) {
-            self->OnRoasted(ec);
-        });
+        roast_timer_.async_wait(
+            net::bind_executor(strand_, [self = shared_from_this()](const sys::error_code &ec) {
+                self->OnRoasted(ec);
+            }));
     }
 
     void OnRoasted(const sys::error_code &ec) {
@@ -109,9 +136,10 @@ private:
 
     void MarinadeOnion() {
         logger_.LogMessage("Start marinading onion");
-        marinade_timer_.async_wait([self = shared_from_this()](sys::error_code ec) {
-            self->OnOnionMarinaded(ec);
-        });
+        marinade_timer_.async_wait(
+            net::bind_executor(strand_, [self = shared_from_this()](const sys::error_code &ec) {
+                self->OnOnionMarinaded(ec);
+            }));
     }
 
     void OnOnionMarinaded(sys::error_code ec) {
@@ -124,7 +152,7 @@ private:
         CheckReadiness(ec);
     }
 
-    void CheckReadiness(sys::error_code ec) {
+    void CheckReadiness(const sys::error_code &ec) {
         if (delivered_) return;
         if (ec) return Deliver(ec);
 
@@ -173,6 +201,7 @@ private:
 
     net::steady_timer roast_timer_{io_, std::chrono::seconds(1)};
     net::steady_timer marinade_timer_{io_, std::chrono::seconds(2)};
+    net::strand<net::io_context::executor_type> strand_{net::make_strand(io_)};
 };
 
 class Restaurant {
